68b364543f6ad86d8b59c1638679467fd39bae25
iamtaufik <muhtopik07@gmail.com>
2024-04-03 23:10:57 +0700

    completed sc

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
 from flask import Flask, request, jsonify
 import pickle
+from datetime import datetime
 
 app = Flask(__name__)
 
-model = pickle.load(open('model_new.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
-@app.route('/api/predict')
+def count_month(start_date, end_date):
+    # start_date = datetime.strptime(start_date, "%Y-%m-%d")
+    start_date = datetime.strptime(start_date, "%m/%d/%Y")
+    end_date = datetime.strptime(end_date, "%m/%d/%Y")
+    months = (end_date.year - start_date.year) * 12 + (end_date.month - start_date.month)
+    
+    if end_date.day < start_date.day:
+        months -= 1
+    
+    return months
+
+@app.route('/')
+def home():
+    return "Hello World!"
+
+@app.route('/api/predict', methods=['POST'])
 def predict():
-    future = model.make_future_dataframe(periods=12, freq='M')
+    request_data = request.get_json()
+    date_to_predict = request_data['date_to_predict']
+
+    month = count_month("10/31/2019", date_to_predict)
+    future = model.make_future_dataframe(periods=month + 1, freq='M')
     forecast = model.predict(future)
 
     array_of_predict = []
